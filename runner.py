d693b4180971555e2f196004c5d4f59609961740
Abusheik008 <abusheikabdulrahim2021@gmail.com>
2024-07-31 16:57:37 +0530

    fixed bugs

--- runner.py
+++ runner.py
@@ -6,24 +6,34 @@ from transformers import PaliGemmaForConditionalGeneration, PaliGemmaProcessor, 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model_id = "model"
-processor = PaliGemmaProcessor.from_pretrained(model_id)
 
+
+print("Planning to launch the model...")
 
 bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_quant_type="nf4",
     bnb_4bit_compute_dtype=torch.bfloat16
 )
+
+print("Loading the pretrained model")
+
 model_4bit = PaliGemmaForConditionalGeneration.from_pretrained(
     model_id,
     quantization_config=bnb_config,
     device_map="auto"
 )
 
+print("loading the processor")
+processor = PaliGemmaProcessor.from_pretrained(model_id)
+
 def generate_caption(prompt, image_path):
+    start_time = time.time()
     raw_image = Image.open(image_path)
     inputs = processor(prompt, raw_image, return_tensors="pt").to(device)
-    output = model_4bit.generate(**inputs, max_new_tokens=20)
+    output = model_4bit.generate(**inputs, max_new_tokens=300)
+    elapsed_time = time.time() - start_time
+    print(elapsed_time, "inference seconds")
     return processor.decode(output[0], skip_special_tokens=True)[len(prompt):]
 
 def main():
