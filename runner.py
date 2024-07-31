4102500a98e803441acad70ac2e10731d934ec1b
Abusheik008 <abusheikabdulrahim2021@gmail.com>
2024-07-31 17:05:50 +0530

    fixed bugs

--- runner.py
+++ runner.py
@@ -7,7 +7,6 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model_id = "model"
 
-
 print("Planning to launch the model...")
 
 bnb_config = BitsAndBytesConfig(
@@ -22,31 +21,33 @@ model_4bit = PaliGemmaForConditionalGeneration.from_pretrained(
     model_id,
     quantization_config=bnb_config,
     device_map="auto"
-)
+).to(device)
 
 print("loading the processor")
 processor = PaliGemmaProcessor.from_pretrained(model_id)
 
 def generate_caption(prompt, image_path):
-    start_time = time.time()
     raw_image = Image.open(image_path)
     inputs = processor(prompt, raw_image, return_tensors="pt").to(device)
-    output = model_4bit.generate(**inputs, max_new_tokens=300)
+    
+    start_time = time.time()
+    with torch.no_grad():  # Disable gradient calculation
+        output = model_4bit.generate(**inputs, max_new_tokens=300)
     elapsed_time = time.time() - start_time
+
     print(elapsed_time, "inference seconds")
     return processor.decode(output[0], skip_special_tokens=True)[len(prompt):]
 
 def main():
-    start_time = time.time()
-
     prompt = "MORE_DETAILED_CAPTION"
     image_file = "test1.jpeg"
+    
+    start_time = time.time()
     caption = generate_caption(prompt, image_file)
+    elapsed_time = time.time() - start_time
 
     print(caption)
-
-    elapsed_time = time.time() - start_time
-    print(elapsed_time, "seconds")
+    print(f"{elapsed_time:.2f} seconds")
     minutes, seconds = divmod(elapsed_time, 60)
     if minutes > 0:
         print(f"{int(minutes)} minutes and {int(seconds)} seconds")
